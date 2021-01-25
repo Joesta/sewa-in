1c1ee53a3ca22bce6604169c79ac7f2d0c16a272
assish <assish@aca3.assish>
2021-01-25 20:38:44 +0530

    loginRegistrationResetPassword

---
+++ app/src/main/java/za/co/robusttech/sewa_in/activities/LoginActivity.java
@@ -0,0 +1,111 @@
+package za.co.robusttech.sewa_in.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import za.co.robusttech.sewa_in.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button btn_login;
+
+    FirebaseAuth auth;
+    TextView forgot_password , sign_in;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btn_login = findViewById(R.id.btn_login);
+        forgot_password = findViewById(R.id.forgot_password);
+        sign_in = findViewById(R.id.sign_in_login);
+
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resetPWD = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(resetPWD);
+            }
+        });
+
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Reg = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(Reg);
+            }
+        });
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = auth.getCurrentUser();
+                if( mFirebaseUser != null){
+                    Toast.makeText(LoginActivity.this, "You Are Logged In", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent (LoginActivity.this, HomeActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Please Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
+                    Toast.makeText(LoginActivity.this, "All fileds are required", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    auth.signInWithEmailAndPassword(txt_email, txt_password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Authentication failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        auth.addAuthStateListener(mAuthStateListener);
+    }
+}
--- app/src/main/java/za/co/robusttech/sewa_in/activities/MainActivity.java
+++ app/src/main/java/za/co/robusttech/sewa_in/activities/MainActivity.java
@@ -2,15 +2,31 @@ package za.co.robusttech.sewa_in.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import za.co.robusttech.sewa_in.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 750;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //splash_screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intro = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intro);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
     }
 }
