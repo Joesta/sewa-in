058f2d84cc0647490de73ac36f702b727ad39e8a
Joesta <joestax@icloud.com>
2021-01-27 04:45:49 +0200

    add customer constants, modifed customer POJO

---
+++ app/src/main/java/za/co/robusttech/sewa_in/constants/ICustomerConstants.java
@@ -0,0 +1,15 @@
+package za.co.robusttech.sewa_in.constants;
+
+/**
+ * Project Name - sewa-in
+ * Created on 2021/01/27 at 4:38 AM
+ */
+
+public interface ICustomerConstants {
+
+    String FIRST_NAME = "firstName";
+    String LAST_NAME = "lastName";
+    String CELLPHONE_NUMBER = "cellphoneNumber";
+    String EMAIL_ADDRESS = "emailAddress";
+    String IMAGE_URL = "imageUrl";
+}
--- app/src/main/java/za/co/robusttech/sewa_in/models/Customer.java
+++ app/src/main/java/za/co/robusttech/sewa_in/models/Customer.java
@@ -1,8 +1,12 @@
 package za.co.robusttech.sewa_in.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import za.co.robusttech.sewa_in.constants.ICustomerConstants;
 
 /**
  * Project Name - sewa-in
@@ -16,7 +20,19 @@ public class Customer {
     private String customerId;
     private String firstName;
     private String lastName;
-    private String cellNumber;
+    private String cellphoneNumber;
     private String emailAddress;
     private String password;
+    private String imageUrl;
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(ICustomerConstants.FIRST_NAME, firstName);
+        map.put(ICustomerConstants.LAST_NAME, lastName);
+        map.put(ICustomerConstants.CELLPHONE_NUMBER, cellphoneNumber);
+        map.put(ICustomerConstants.EMAIL_ADDRESS, emailAddress);
+        map.put(ICustomerConstants.IMAGE_URL, imageUrl);
+
+        return map;
+    }
 }
